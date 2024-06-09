23c115d7c23d212aee235313d0d7b020fcbdd317
Yullia Kravchuk <ljuljek554@gmail.com>
2024-06-09 14:24:00 +0300

    fix createContactSchema.contactType

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -9,7 +9,7 @@ export const createContactSchema = Joi.object({
     .min(3)
     .max(20)
     .valid('work', 'home', 'personal')
-    .required(),
+    .default('personal'),
 });
 
 export const updateContactSchema = Joi.object({
