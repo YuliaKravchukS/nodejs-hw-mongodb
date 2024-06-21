4347544809d3fa8378c843741d55864cba818ee4
Yullia Kravchuk <ljuljek554@gmail.com>
2024-06-21 17:06:57 +0300

    fix import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import { TEMP_UPLOAD_DIR } from './constants/index.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
-import { TEMP_UPLOAD_DIR } from '../constants/index.js';
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 
 const bootstrap = async () => {
