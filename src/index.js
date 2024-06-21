ac164b09dca15e7a38129e5399f86de13ac82ddc
Yullia Kravchuk <ljuljek554@gmail.com>
2024-06-21 17:03:56 +0300

    add util createDirIfNotExists

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
+import { TEMP_UPLOAD_DIR } from '../constants/index.js';
+import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
+  await createDirIfNotExists(TEMP_UPLOAD_DIR);
   setupServer();
 };
 
